feae9b21c6092a972689632ad50020267ebd2b83
houmingyuan <houmingyuan@gmail.com>
2023-12-14 16:24:29 +0800

    set demo

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+	var URL_TO_PLUGIN = "http://houmingyuan.github.com/onlyofice.github.io/sdkjs-plugins/content/biyue/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
