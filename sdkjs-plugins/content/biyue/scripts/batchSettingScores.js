fca920ef0427a4fd346c8d5f448c5e984258a4e7
叶鹭鹏 <yelupeng@dcx.com>
2024-07-22 16:36:12 +0800

    EDU-14182 feat：onlyoffice 处理批量设置分数窗口

--- sdkjs-plugins/content/biyue/scripts/batchSettingScores.js
+++ sdkjs-plugins/content/biyue/scripts/batchSettingScores.js
@@ -1,5 +1,7 @@
 ;(function (window, undefined) {
   let biyueCustomData = {}
+  let question_map = {}
+  let questionList = []
   window.Asc.plugin.init = function () {
     console.log('examExport init')
     window.Asc.plugin.sendToPlugin('onWindowMessage', { type: 'PaperMessage' })
@@ -7,28 +9,89 @@
 
   function init() {
     let node_list = biyueCustomData.node_list || []
-    let question_map = biyueCustomData.question_map || {}
+    question_map = biyueCustomData.question_map || {}
     let html = ''
+    let question_list = []
     for (const key in node_list) {
       let item = question_map[node_list[key].id] || ''
       if (node_list[key].level_type == 'question') {
         if (item && item.question_type !== 6) {
-          html += `<span class="question">${(item.ques_default_name ? item.ques_default_name : '') + '.'}`
+          html += `<span class="question">${(item.ques_default_name ? item.ques_default_name : '')}`
           if (item.ask_list.length > 0) {
-            for (const key in item.ask_list) {
-              html += `<input type="text" class="score score-${item.ask_list[key].id}" value="${item.ask_list[key].score || 0}">`
+            for (const ask_k in item.ask_list) {
+              html += `<input type="text" class="score ques-${ node_list[key].id } ask-index-${ask_k} ask-${item.ask_list[ask_k].id}" value="${item.ask_list[ask_k].score || 0}">`
             }
           } else {
-            html += `<input type="text" class="score score-${ node_list[key].id }" value="${ node_list[key].score || 0 }">`
+            html += `<input type="text" class="score ques-${ node_list[key].id }" value="${ item.score || 0 }">`
           }
           html += `</span>`
+
+          if (!question_list.includes(node_list[key].id)) {
+            question_list.push(node_list[key].id)
+          }
         }
       } else if (node_list[key].level_type == 'struct' && item){
         html += `<div class="group">${ item.text.split('\r\n')[0] }</div>`
       }
     }
     $('.batch-setting-score-info').html(html)
-    console.log('html:', html)
+    $('#confirm').on('click', onConfirm)
+    questionList = question_list || []
+  }
+
+  function onConfirm() {
+    for (const key in questionList) {
+      let id = questionList[key] || ''
+      let dom = $('.ques-'+id)
+      if (id && question_map[id] && dom) {
+        let ask_list = question_map[id].ask_list || []
+        if (ask_list.length > 0) {
+          // 有小问区的题
+          let sumScore = 0
+          for (const k in ask_list) {
+            let ask_dom = $(`.ques-${ id }.ask-index-${ k }`)
+            if (ask_dom) {
+              let params = {
+                id: id,
+                type: 'ask',
+                index: k,
+                score: ask_dom.val()
+              }
+              sumScore += params.score * 1
+              changeScore(params)
+            }
+          }
+          // 题目的分数设置为小问的分数的和
+          let params = {
+            id: id,
+            type: 'question',
+            score: sumScore
+          }
+          changeScore(params)
+        } else {
+          // 没有小问区的题目
+          let params = {
+            id: id,
+            type: 'question',
+            score: dom.val()
+          }
+          changeScore(params)
+        }
+      }
+    }
+    // 将窗口的信息传递出去
+    window.Asc.plugin.sendToPlugin('onWindowMessage', {
+      type: 'changeQuestionMap',
+      data: question_map,
+    })
+  }
+
+  function changeScore({id, type, index, score}) {
+    if (type == 'ask') {
+      question_map[id].ask_list[index].score = score
+    } else {
+      question_map[id].score = score
+    }
   }
 
   window.Asc.plugin.attachEvent('initPaper', function (message) {
