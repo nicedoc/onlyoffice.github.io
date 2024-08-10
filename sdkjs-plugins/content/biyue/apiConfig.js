dd67d386cd2175aa0a4806226fb0967b3017fbcd
叶鹭鹏 <yelupeng@dcx.com>
2024-08-10 11:53:43 +0800

    oper: 修改分支配置

--- sdkjs-plugins/content/biyue/apiConfig.js
+++ sdkjs-plugins/content/biyue/apiConfig.js
@@ -1,6 +1,6 @@
 
 // base api
-const VUE_APP_GO_API = 'https://eduques.xmdas-link.com/bonlyoffice'
-const VUE_APP_AUTH_API = 'https://edusys.xmdas-link.com/bonlyoffice'
+const VUE_APP_GO_API = 'https://eduques.xmdas-link.com'
+const VUE_APP_AUTH_API = 'https://edusys.xmdas-link.com'
 
 export { VUE_APP_GO_API, VUE_APP_AUTH_API }
