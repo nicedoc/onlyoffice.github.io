c95474b3bc3a9b6310ffeb956eb0da0f23963cc2
叶鹭鹏 <yelupeng@dcx.com>
2024-08-20 11:41:05 +0800

    EDU-14182 feat：onlyoffice 批量设置窗口 题号鼠标悬浮的时候显示题目text

--- sdkjs-plugins/content/biyue/scripts/batchSettingQuestionType.js
+++ sdkjs-plugins/content/biyue/scripts/batchSettingQuestionType.js
@@ -37,7 +37,7 @@
       let item = question_map[node_list[key].id] || ''
       if (node_list[key].level_type == 'question') {
         if (item) {
-          html += `<span class="question">${(item.ques_name || item.ques_default_name || '')}`
+          html += `<span class="question" title="${ item.text }">${(item.ques_name || item.ques_default_name || '')}`
           html += `<select class="type-item ques-${ node_list[key].id }">`
           html += `<option value="" style="display:none;"></option>`
           for (const key in question_type_options) {
