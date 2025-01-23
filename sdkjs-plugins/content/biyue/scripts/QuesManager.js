a68826c56464886ee4f9ff7d7fad091939bc269e
lvliying <lvliying@dcx.com>
2025-01-23 19:40:18 +0800

    RP-117 fix: 添加非空判断

--- sdkjs-plugins/content/biyue/scripts/QuesManager.js
+++ sdkjs-plugins/content/biyue/scripts/QuesManager.js
@@ -1729,7 +1729,7 @@ function handleChangeType(res, res2) {
 		.then(() => {
 			return imageAutoLink(typequesId, false)
 		}).then((res3) => {
-			if (res3.rev) {
+			if (res3 && res3.rev) {
 				return ShowLinkedWhenclickImage({
 					client_id: typequesId
 				})
