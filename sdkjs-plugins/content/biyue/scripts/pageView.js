dc998154b1dd800f926e7048eaa448b60264fc06
gongjisen <1845553629@qq.com>
2025-03-04 18:58:26 +0800

    修改待处理图片面积关联时的滚动条，未关联列表的修改及忽略恢复按钮的点击

--- sdkjs-plugins/content/biyue/scripts/pageView.js
+++ sdkjs-plugins/content/biyue/scripts/pageView.js
@@ -416,7 +416,8 @@ function showPanelLink() {
 			type:'panelRight',
 			icons:['resources/light/img.png']
 		}, 'pictureIndexMessage', {
-			list: res.list
+			list: res.list,
+			list_ignore: res.list_ignore
 		})
 	})
 	// showCom('#panelLink', true)
